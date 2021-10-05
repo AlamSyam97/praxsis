a236c9e23b0b795ef8d716da5bd7b74ec778bfa9
AlamSyam97 <alamsyam9703@gmail.com>
2021-10-05 13:47:39 +0700

    pertemuan type data

---
+++ 1-2/tipe-data.py
@@ -0,0 +1,45 @@
+#data tunggal
+x ="welcome to praxsis 2021"
+print(x)
+print (type(x))  
+
+#data int
+ex=99
+print(ex)
+print (type(ex))
+
+# float
+y=20.9
+print(y)
+print (type(y))
+
+# boolean
+s= True
+print(s)
+print (type(s))
+
+# List 
+n= ["ALAM","SODIQ","YASIN"]
+print(n)
+print (type(n))
+
+#tuple
+j=("1","2","3","4","5")
+print(j)
+print (type(j))
+
+#dict
+
+k={
+ 
+ "Nama":"ahmad syamsul hidayat",
+ "nim" :"181111118",
+ "angkatan":2018
+}
+
+
+
+
+print(k)
+print (type(k))
+print(len(k)) #menghitung jumlah data
