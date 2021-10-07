30f88af11193fb13f7e0b76da8563626ce3afa0c
AlamSyam97 <alamsyam9703@gmail.com>
2021-10-07 17:21:43 +0700

    developer django dan css

--- 1-2/tipe-data.py
+++ 1-2/tipe-data.py
@@ -2,33 +2,39 @@
 x ="welcome to praxsis 2021"
 print(x)
 print (type(x))  
+#tipe data yang menggunakan huruf dan berbagai karakter ,dengan menggunakan tanda petik dubel atau tunggal
 
 #data int
 ex=99
 print(ex)
 print (type(ex))
-
+#type data nomerik
+ 
 # float
 y=20.9
 print(y)
 print (type(y))
+#type data yang mempunyai koma 
 
 # boolean
 s= True
 print(s)
 print (type(s))
+#menentukan type data true dan false
 
 # List 
 n= ["ALAM","SODIQ","YASIN"]
 print(n)
 print (type(n))
+#type data yang menampung beberapa nilai
 
 #tuple
 j=("1","2","3","4","5")
 print(j)
 print (type(j))
+#berbagai tipe data yang gak bisa diubah
 
-#dict
+#dictionery
 
 k={
  
@@ -43,3 +49,4 @@ k={
 print(k)
 print (type(k))
 print(len(k)) #menghitung jumlah data
+
--- 1-3/alamsite/polls/views.py
+++ 1-3/alamsite/polls/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render 
+from . import models
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello alam , praxsis admin 2021. You're at the polls index.")
+    if request.POST:
+        input=request.POST["nama"]
+        models.tugas.objects.create(nama=input)
+    data=models.tugas.objects.all()
+        
+    return render(request,'index.html',{
+        "data":data
+    })
