7e289ed5db74d6cb412bbdcb09778c8482d9d792
AlamSyam97 <alamsyam9703@gmail.com>
2021-10-08 17:19:08 +0700

    meneruskan projek django edit,hapus

--- 1-3/alamsite/polls/urls.py
+++ 1-3/alamsite/polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('hapus/<id>', views.hapus,),
+    path('edit/<id>', views.edit,),
+    path('detail/<id>', views.detail,),
 ]
--- 1-3/alamsite/polls/views.py
+++ 1-3/alamsite/polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render 
+# from typing_extensions import Required
+from django.shortcuts import render, redirect
 from . import models
 # Create your views here.
 from django.http import HttpResponse
@@ -12,4 +13,28 @@ def index(request):
         
     return render(request,'index.html',{
         "data":data
+    })
+
+def hapus(request, id):
+    models.tugas.objects.filter(id = id).delete()
+    return redirect('/')
+
+def edit(request, id):
+    if request.POST:
+        input = request.POST['nama']
+        print(input)
+        models.tugas.objects.filter(pk = id).update(nama = input)
+        return redirect('/')
+
+    hasil = models.tugas.objects.filter(pk = id).first()
+    print(hasil)
+    return render(request,'edit.html',{
+        'detailedit' : hasil,
+
+def detail(request, id):
+    data = models.tugas.objects.filter(pk = id).first()
+    print(data)
+    return render(request,'detail.html',{
+        "detailedit" :data 
+
     })
